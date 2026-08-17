0fd52a416349ea293a4061c748c8629708a9ed4b
0eroiQ <elvissalihovic.es@gmail.com>
2026-08-17 20:37:55 +0800

    Mount Work local coding model integration

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import WorkIntegration from "./components/WorkIntegration";
+import WorkCodingModelsIntegration from "./components/WorkCodingModelsIntegration";
 import UpdateIntegration from "./components/UpdateIntegration";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <WorkIntegration />
+    <WorkCodingModelsIntegration />
     <UpdateIntegration />
   </React.StrictMode>,
 );
