de98d4a266fa6ca91b76f1f58af100b3924df486
0eroiQ <elvissalihovic.es@gmail.com>
2026-08-17 20:56:38 +0800

    Mount whole-project Work agent integration

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import WorkIntegration from "./components/WorkIntegration";
 import WorkCodingModelsIntegration from "./components/WorkCodingModelsIntegration";
+import WorkAgentIntegration from "./components/WorkAgentIntegration";
 import UpdateIntegration from "./components/UpdateIntegration";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
     <WorkIntegration />
     <WorkCodingModelsIntegration />
+    <WorkAgentIntegration />
     <UpdateIntegration />
   </React.StrictMode>,
 );
