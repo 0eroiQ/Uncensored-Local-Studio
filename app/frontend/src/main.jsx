e49865486817e6677e72ef66024d53290a72ce71
0eroiQ <elvissalihovic.es@gmail.com>
2026-08-17 20:06:56 +0800

    Mount USB updater in Settings

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import WorkIntegration from "./components/WorkIntegration";
+import UpdateIntegration from "./components/UpdateIntegration";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <WorkIntegration />
+    <UpdateIntegration />
   </React.StrictMode>,
 );
