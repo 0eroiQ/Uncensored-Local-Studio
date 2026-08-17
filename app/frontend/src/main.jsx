4247308e240a722754a9192cd71f97756a78c2ac
0eroiQ <elvissalihovic.es@gmail.com>
2026-08-17 22:24:18 +0800

    Mount Text Chat-style Work chrome

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import WorkIntegration from "./components/WorkIntegration";
+import WorkTextChatChrome from "./components/WorkTextChatChrome";
 import WorkCodingModelsIntegration from "./components/WorkCodingModelsIntegration";
 import WorkDownloadMonitor from "./components/WorkDownloadMonitor";
 import WorkAgentIntegration from "./components/WorkAgentIntegration";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <WorkIntegration />
+    <WorkTextChatChrome />
     <WorkCodingModelsIntegration />
     <WorkDownloadMonitor />
     <WorkAgentIntegration />
